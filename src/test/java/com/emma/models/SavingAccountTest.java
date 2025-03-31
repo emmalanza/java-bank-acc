97431a3e6c67fe29ff7ed36686186ab1d73204d5
Emma <emmalanza@hotmail.com>
2025-03-31 11:29:34 +0200

    test monthly fee for extra withdrawals: failed

--- src/test/java/com/emma/models/SavingAccountTest.java
+++ src/test/java/com/emma/models/SavingAccountTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
-    
+
     @Test
     void testCannotDepositWhenIsInactive() {
         SavingAccount account = new SavingAccount(5000, 5);
@@ -18,5 +18,18 @@ public class SavingAccountTest {
         SavingAccount account = new SavingAccount(5000, 5);
         account.withdraw(2000);
         assertEquals(5000, account.balance);
+    }
+
+    @Test
+    void testFeeForExtraWithdrawals() {
+        SavingAccount account = new SavingAccount(15000, 12);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.monthlyStatement();
+        assertEquals(4040, account.balance);
+        assertEquals(1000, account.monthlyFee);
     }
 }
