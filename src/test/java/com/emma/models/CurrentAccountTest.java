70e6334d55391f36d273927d3ccafd7633314b67
Emma <emmalanza@hotmail.com>
2025-03-31 12:16:11 +0200

    test current acc print: failed

--- src/test/java/com/emma/models/CurrentAccountTest.java
+++ src/test/java/com/emma/models/CurrentAccountTest.java
@@ -2,6 +2,9 @@ package com.emma.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 public class CurrentAccountTest {
@@ -21,6 +24,27 @@ public class CurrentAccountTest {
         account.deposit(1000);
         assertEquals(0, account.balance);
         assertEquals(0, account.getOverdraft());
+    }
+
+    @Test
+    void testPrint() {
+        CurrentAccount account = new CurrentAccount(15000, 12);
+        account.withdraw(2000);
+        account.deposit(1000);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        account.print();
+
+        String expectedOutput = "Balance: 14000.0\n" +
+                "Monthly Fee: 0.0\n" +
+                "Total transactions: 2\n" +
+                "Overdraft: 0.0\n";
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        System.setOut(System.out);
     }
     
 }
