4bb0865f44964c03844c37373efe5c2cbd7049fd
Emma <emmalanza@hotmail.com>
2025-03-31 10:56:11 +0200

    create saving acc and implement deposit method: test passed

---
+++ src/main/java/com/emma/models/SavingAccount.java
@@ -0,0 +1,20 @@
+package com.emma.models;
+
+public class SavingAccount extends Account {
+
+    private boolean active;
+
+    public SavingAccount(double balance, double annualRate) {
+        super(balance, annualRate);
+        this.active = balance>= 10000;
+
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (active) {
+            super.deposit(amount);
+        }
+    }
+    
+}
