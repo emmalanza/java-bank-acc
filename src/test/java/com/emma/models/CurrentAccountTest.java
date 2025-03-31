f01239dc6fc677e3eba138e95b52d0c5ba4d685f
Emma <emmalanza@hotmail.com>
2025-03-31 11:55:26 +0200

    test current acc creates overdraft on withdraw: failed

---
+++ src/test/java/com/emma/models/CurrentAccountTest.java
@@ -0,0 +1,17 @@
+package com.emma.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CurrentAccountTest {
+
+    @Test
+    void testWithdrawCreatesOverdraft() {
+        CurrentAccount account = new CurrentAccount(5000, 5);
+        account.withdraw(6000);
+        assertEquals(0, account.balance);
+        assertEquals(1000, account.getOverdraft());
+    }
+    
+}
