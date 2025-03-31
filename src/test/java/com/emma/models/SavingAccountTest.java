2fdfd9090925b6f0577eddbf6e6c196134e9f829
Emma <emmalanza@hotmail.com>
2025-03-31 10:51:00 +0200

    test saving acc deposit when is inactive: failed

---
+++ src/test/java/com/emma/models/SavingAccountTest.java
@@ -0,0 +1,15 @@
+package com.emma.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SavingAccountTest {
+    
+    @Test
+    void testCannotDepositWhenIsInactive() {
+        SavingAccount account = new SavingAccount(5000, 5);
+        account.deposit(2000);
+        assertEquals(5000, account.balance);
+    }
+}
