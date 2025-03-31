12a5ce7a12f347cf66bc5c18ffb773b306b09fc6
Emma <emmalanza@hotmail.com>
2025-03-31 09:18:03 +0200

    create account class and implement deposit method: test passed

---
+++ src/main/java/com/emma/models/Account.java
@@ -0,0 +1,23 @@
+package com.emma.models;
+
+public class Account {
+
+    protected double balance;
+    protected int numDeposits = 0;
+    protected int numWithdrawals = 0;
+    protected double annualRate;
+    protected double monthlyFee = 0.0;
+
+    public Account(double balance, double annualRate) {
+        this.balance = balance;
+        this.annualRate = annualRate;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            numDeposits++;
+        }
+    }
+    
+}
