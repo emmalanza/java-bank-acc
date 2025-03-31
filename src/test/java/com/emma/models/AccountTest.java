0bb1e1487ab957b047fc2a05da4755de0914a409
Emma <emmalanza@hotmail.com>
2025-03-31 09:28:46 +0200

    implement beforeEach and test withdraw method: failed

--- src/test/java/com/emma/models/AccountTest.java
+++ src/test/java/com/emma/models/AccountTest.java
@@ -2,14 +2,33 @@ package com.emma.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
+
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account(5000, 5);
+    }
     @Test
     void testDeposit() {
-        Account account = new Account(5000, 5);
         account.deposit(2000);
         assertEquals(7000, account.balance);
     }
-    
+
+    @Test 
+    void testDepositNegativeAmount() {
+        account.deposit(-10);
+        assertEquals(5000, account.balance);
+    }
+
+    @Test
+    void testWithdraw() {
+        account.withdraw(2000);
+        assertEquals(3000, account.balance);
+    }
+
 }
