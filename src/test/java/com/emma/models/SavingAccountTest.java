f0742fba616e7b444d779f562ce8c06d3a5f920d
Emma <emmalanza@hotmail.com>
2025-03-31 11:41:07 +0200

    test saving acc print: failed

--- src/test/java/com/emma/models/SavingAccountTest.java
+++ src/test/java/com/emma/models/SavingAccountTest.java
@@ -2,6 +2,9 @@ package com.emma.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
@@ -31,5 +34,29 @@ public class SavingAccountTest {
         account.monthlyStatement();
         assertEquals(4040, account.balance);
         assertEquals(1000, account.monthlyFee);
+    }
+
+    @Test
+    void testPrint() {
+        SavingAccount account = new SavingAccount(15000, 12);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.withdraw(2000);
+        account.monthlyStatement();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        account.print();
+
+        String expectedOutput = "Balance: 4040.0\n" +
+                "Monthly Fee: 1000.0\n" +
+                "Total transactions: 5\n";
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        System.setOut(System.out);
     }
 }
